bccc65c28bc70eccad2484b96fe4ef5857e1f8fa
nischal <shakyanischal1111@gmail.com>
2024-05-08 14:25:55 +0545

    lazyLoading

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useState, Suspense, lazy } from 'react';
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import Weather from './pages/Weather'
+import Loading from './components/Loading';
+// import Weather from './pages/Weather'
+const LazyWeather = lazy(() => import('./pages/Weather'));
 
 
 function App() {
@@ -11,10 +13,17 @@ function App() {
     <>
     <Routes>
      <Route path='/' element={<Home/>}/>
-     <Route path='/weather' element={<Weather/>}/>
+     
+     <Route path='/weather' element={
+     <Suspense fallback={<Loading />}>
+     <LazyWeather/>
+     </Suspense>
+     }/>
+     {/* <Route path='/weather' element={<Weather/>}></Route> */}
      </Routes>
     </>
   )
 }
+
 
 export default App
