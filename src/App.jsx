b6870a3ead25fea648bfeaf63a8bc459d60003c7
Nischal Shakya <shakyanischal1111@gmail.com>
2024-05-31 11:41:03 +0545

    new

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <>
     <Routes>
      <Route path='/' element={<Home/>}/>
-     
+
      <Route path='/weather' element={
      <Suspense fallback={<Loading />}>
      <LazyWeather/>
